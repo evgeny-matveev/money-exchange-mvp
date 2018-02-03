d0218c30fc29e7ee9a650efe9a51dc6699a33b5f
Evgeny Matveev <evgeny@mak-design.me>
2018-02-03 17:27:20 +0300

    setup layout

--- dist/money-exchange-mvp.js
+++ dist/money-exchange-mvp.js
@@ -100,7 +100,7 @@ function getBaseURL(url) {
 exports.getBundleURL = getBundleURLCached;
 exports.getBaseURL = getBaseURL;
 
-},{}],5:[function(require,module,exports) {
+},{}],6:[function(require,module,exports) {
 var bundle = require('./bundle-url');
 
 function updateLink(link) {
@@ -132,19 +132,19 @@ function reloadCSS() {
 
 module.exports = reloadCSS;
 
-},{"./bundle-url":8}],6:[function(require,module,exports) {
+},{"./bundle-url":8}],7:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"_css_loader":5}],4:[function(require,module,exports) {
+},{"_css_loader":6}],5:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"_css_loader":5}],2:[function(require,module,exports) {
+},{"_css_loader":6}],4:[function(require,module,exports) {
 'use strict';
 
 require('normalize.css');
@@ -154,7 +154,7 @@ var _index = require('./index.scss');
 var _index2 = _interopRequireDefault(_index);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-},{"normalize.css":6,"./index.scss":4}],9:[function(require,module,exports) {
+},{"normalize.css":7,"./index.scss":5}],9:[function(require,module,exports) {
 
 var global = (1, eval)('this');
 var OldModule = module.bundle.Module;
@@ -174,7 +174,7 @@ module.bundle.Module = Module;
 
 if (!module.bundle.parent && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
-  var ws = new WebSocket('ws://' + hostname + ':' + '59820' + '/');
+  var ws = new WebSocket('ws://' + hostname + ':' + '59959' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -275,5 +275,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.require, id);
   });
 }
-},{}]},{},[9,2])
+},{}]},{},[9,4])
 //# sourceMappingURL=/dist/money-exchange-mvp.map
