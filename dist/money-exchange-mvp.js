8cc606aef88fffa812ad325c6b65da2731893835
Evgeny Matveev <evgeny@mak-design.me>
2018-02-06 14:32:45 +0300

    improve UX for select: add an arrow icon

--- dist/money-exchange-mvp.js
+++ dist/money-exchange-mvp.js
@@ -69,7 +69,7 @@ require = (function (modules, cache, entry) {
 
   // Override the current require with this new one
   return newRequire;
-})({11:[function(require,module,exports) {
+})({9:[function(require,module,exports) {
 var bundleURL = null;
 function getBundleURLCached() {
   if (!bundleURL) {
@@ -132,7 +132,7 @@ function reloadCSS() {
 
 module.exports = reloadCSS;
 
-},{"./bundle-url":11}],10:[function(require,module,exports) {
+},{"./bundle-url":9}],7:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
@@ -144,7 +144,7 @@ module.exports = reloadCSS;
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"./assets/top.png":[["ac9ba8d75c6840ca1b7fe32fabacc657.png",6],6],"./assets/bot.png":[["fcd9f2661a1602ac780598f20c2ed7bd.png",5],5],"_css_loader":4}],9:[function(require,module,exports) {
+},{"./assets/top.png":[["ac9ba8d75c6840ca1b7fe32fabacc657.png",5],5],"./assets/bot.png":[["fcd9f2661a1602ac780598f20c2ed7bd.png",6],6],"_css_loader":4}],8:[function(require,module,exports) {
 /*!
  * money.js / fx() v0.2
  * Copyright 2014 Open Exchange Rates
@@ -372,7 +372,7 @@ document.addEventListener('DOMContentLoaded', function (e) {
     firstInput.value = exchangeMoney(secondInput.value, true);
   };
 });
-},{"normalize.css":10,"./index.scss":3,"money":9}],12:[function(require,module,exports) {
+},{"normalize.css":7,"./index.scss":3,"money":8}],16:[function(require,module,exports) {
 
 var global = (1, eval)('this');
 var OldModule = module.bundle.Module;
@@ -392,7 +392,7 @@ module.bundle.Module = Module;
 
 if (!module.bundle.parent && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
-  var ws = new WebSocket('ws://' + hostname + ':' + '50690' + '/');
+  var ws = new WebSocket('ws://' + hostname + ':' + '57403' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -493,5 +493,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.require, id);
   });
 }
-},{}]},{},[12,2])
+},{}]},{},[16,2])
 //# sourceMappingURL=/dist/money-exchange-mvp.map
